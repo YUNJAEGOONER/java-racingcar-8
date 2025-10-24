88dfa65f5adfa0c534d24c31254f0832933edeb8
YUNJAEGOONER <coygyj@gmail.com>
2025-10-24 15:44:16 +0900

    feat: 자동차 주행 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,10 +1,14 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
 
     private String name;
 
     private int distance = 0;
+
+    private static int threshold = 4;
 
     public Car(String name){
         this.name = name;
@@ -16,6 +20,12 @@ public class Car {
 
     public int getDistance() {
         return distance;
+    }
+
+    public void drive(){
+        if (Randoms.pickNumberInRange(0, 9) >= threshold){
+            this.distance ++;
+        }
     }
 
 }
