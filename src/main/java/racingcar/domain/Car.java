e21cb0db691de870f4df266dc58dc64b2f8ca750
YUNJAEGOONER <coygyj@gmail.com>
2025-10-24 15:40:27 +0900

    feat: 자동차 객체 구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,21 @@
+package racingcar.domain;
+
+public class Car {
+
+    private String name;
+
+    private int distance = 0;
+
+    public Car(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+}
